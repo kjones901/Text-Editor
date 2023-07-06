e581e01e0549c431a41559264482db12f2c0d659
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-07-06 15:15:19 -0600

    finished adding the rest of the todo's. Still some bugs that need debugging

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,55 +1,58 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+// *Done TODO: Add and configure workbox plugins for a service worker and manifest file.
+// *Done TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: "./index.html",
       }),
       new WebpackPwaManifest({
-        name: 'Text Editor',
-        short_name: 'Text Editor',
-        description: 'Edit your text',
-        start_url: '/',
-        publicPath: './',
-        background_color: '#ffffff',
+        name: "Text Editor",
+        short_name: "Text Editor",
+        description: "Edit your text",
+        start_url: "/",
+        publicPath: "./",
+        background_color: "#ffffff",
         icons: [
           {
-            src: path.resolve('images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
           },
         ],
       }),
       new InjectManifest({
-        swSrc: './src/service-worker.js',
-        swDest: 'service-worker.js',
+        swSrc: "./src-sw.js",
+        swDest: "./src-sw.js",
       }),
     ],
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
             },
           },
         },
